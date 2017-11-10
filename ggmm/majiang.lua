1ac0faf2fb22544347c1ee260520dd776998f2f4
Jeep <1121838283@qq.com>
2017-11-11 01:22:09 +0800

    7d 算法

--- ggmm/majiang.lua
+++ ggmm/majiang.lua
@@ -116,12 +116,13 @@ local BB = {
 }
 local __tongji = mjlib.tongji
 local __floor = math.floor
+local __fmod = math.fmod
 local __combine = mjlib.combine
-function CMD.ting(pai)
+function CMD.tingA(pai)
 	local t,tc = __tongji(pai)
-	return CMD.ting2(t,tc)
+	return CMD.tingA__(t,tc)
 end	
-function CMD.ting2(t,tc)
+function CMD.tingA__(t,tc)
 	local cc={0,1,1,0,1,1,0,1,1,0,1,1,0,1, 1,0,1,1,0,1}
 	cc[0] = 1
 	cc[-1] = 0
@@ -163,145 +164,231 @@ function CMD.ting2(t,tc)
 	local rc = 0
 	local p_out = 0
 	local p_in = 0
-if(true) then	
-	--迭代自己+-
-	for i=1,4 do
-		local vb = t[i]
-		local v = vb
-		if(v>0) then
-			for m=9,1,-1 do
-				local v1 = __floor(v/BB[m])
-				v = v - v1*BB[m]
-				if v1>0 then
-					local v2 = vb - BB[m]
-					p_out = __combine(i,m)
-					---------------------------	
-					local vv = vb
-					for n=9,1,-1 do
-						local v3 = v2+BB[n]
-						local tcp = {t[1],t[2],t[3],t[4]}
-						tcp[i]=v3 
-						-- skynet.error(i,m,n,v3)
-						if(CMD.hu2(tcp)) then							
-							p_in = __combine(i,n)
-							-- table.insert(rr,p_out.."--"..p_in)
-							rr[rc+1] = p_out.."--"..p_in
-							rc = rc+1
+	if(true) then	
+		--迭代自己减一个加一个
+		for i=1,4 do
+			local vb = t[i]
+			local v = vb
+			if(v>0) then
+				for m=9,1,-1 do
+					local v1 = __floor(v/BB[m])
+					v = v - v1*BB[m]
+					if v1>0 then
+						local v2 = vb - BB[m]
+						p_out = __combine(i,m)
+						---------------------------	
+						local vv = vb
+						for n=9,1,-1 do
+							local v3 = v2+BB[n]
+							local tcp = {t[1],t[2],t[3],t[4]}
+							tcp[i]=v3 
+							-- skynet.error(i,m,n,v3)
+							if(CMD.huA__(tcp)) then							
+								p_in = __combine(i,n)
+								-- table.insert(rr,p_out.."--"..p_in)
+								rr[rc+1] = p_out.."--"..p_in
+								rc = rc+1
+							end
 						end
 					end
 				end
 			end
 		end
-	end
-end	
-if(true) then
-	--迭代自己-其他+
-	for i=1,4 do
-		local vb = t[i]
-		local v = vb
-		--可减
-		if(ot[i][1]>0 and vb>0) then
-			--逐个sub
-			local v = t[i]
-			for m=9,1,-1 do
-				local v1 = __floor(v/BB[m])
-				v = v - v1*BB[m]
-				if v1>0 then
-					local v2 = vb - BB[m]
-					--检测有效
-					if CMD.get(i,v2) then
-						p_out = __combine(i,m)
-						--逐一尝试
-						for j=1,4 do
-							--可加
-							if (ot[j][2]>0 and t[j]>0 and i~=j) then
-								--逐个add
-								for n=1,9 do
-									local v3 = BB[n]
-									local tcp = {t[1],t[2],t[3],t[4]}
-									tcp[i]=v2
-									tcp[j]=tcp[j] + v3 
-									if(CMD.hu2(tcp)) then
-										p_in = __combine(j,n)
-										-- table.insert(rr,p_out.."--"..p_in)
-										rr[rc+1] = p_out.."--"..p_in
-										rc = rc+1
+	end	
+	if(true) then
+		--迭代 自己减一个 其他加一个
+		for i=1,4 do
+			local vb = t[i]
+			local v = vb
+			--可减
+			if(ot[i][1]>0 and vb>0) then
+				--逐个sub
+				local v = t[i]
+				for m=9,1,-1 do
+					local v1 = __floor(v/BB[m])
+					v = v - v1*BB[m]
+					if v1>0 then
+						local v2 = vb - BB[m]
+						--检测有效
+						if CMD.get(i,v2) then
+							p_out = __combine(i,m)
+							--逐一尝试
+							for j=1,4 do
+								--可加
+								if (ot[j][2]>0 and t[j]>0 and i~=j) then
+									--逐个add
+									for n=1,9 do
+										local v3 = BB[n]
+										local tcp = {t[1],t[2],t[3],t[4]}
+										tcp[i]=v2
+										tcp[j]=tcp[j] + v3 
+										if(CMD.huA__(tcp)) then
+											p_in = __combine(j,n)
+											-- table.insert(rr,p_out.."--"..p_in)
+											rr[rc+1] = p_out.."--"..p_in
+											rc = rc+1
+										end
 									end
 								end
 							end
+						else
+								
 						end
-					else
-							
-					end
-				end	
-			end
-		end	
+					end	
+				end
+			end	
+		end
+	end	
+	if(#rr>0) then
+		return rr
 	end
-end	
+end
+function CMD.huA(pai)
+	local t,_= __tongji(pai)
+	return CMD.huA__(t)
+end
+function CMD.huA__(t)
+	local flag = true
+	local jc = 0
+	-- util.dump(t,"CMD.hu")
+	-- skynet.error(mjlib.i2str(t[1]))
+	-- skynet.error(mjlib.i2str(t[2]))
+	-- skynet.error(mjlib.i2str(t[3]))
+	-- skynet.error(mjlib.i2str(t[4]))
+	for i=1,4 do
+		if t[i]>0 then
+			-- local key = mjlib.i2str(t[i])
+			local key = t[i]
+			local r  = CMD.get(i,key)
+			if(not r) then
+				flag = false
+				break
+			else
+				jc = jc + r
+			end
+		end
+	end
+	return flag and jc==1
+end
+
+function CMD.hu7D(pai)
+	local t,tc= __tongji(pai)
+	return CMD.hu7D__(t,tc)
+end
+function CMD.hu7D__(t,tc)
+	local flag = true
+	-- local all = 0
+	-- for i=1,4 do
+	-- 	all = all + tc[i]
+	-- end	
+	-- --无吃无碰无杠
+	-- if(all~=14) then
+	-- 	return false
+	-- end
+	-- --数量偶数判断
+	-- for i=1,4 do
+	-- 	all = all + tc[i]
+	-- 	flag = flag and __fmod(tc[i],2)==0
+	-- end
+	-- if(not flag) then
+	-- 	return flag
+	-- end
+	--判断每个数量
+	for i=1,4 do
+		local v = t[i]
+		while v>0 do
+			if(__fmod(v,2)==1) then
+				flag = false
+				break
+			end
+			v = __floor(v/10) 
+		end
+		if flag == false then
+			break
+		end
+	end
+	return flag
+end
+function CMD.ting7D(pai)
+	local t,tc= __tongji(pai)
+	return CMD.ting7D__(t,tc)
+end
+function CMD.ting7D__(t,tc)
+	local all = 0
+	for i=1,4 do
+		all = all + tc[i]
+	end	
+	--无吃无碰无杠
+	if(all~=14) then
+		return false
+	end
+	------------------------------------
+	local ctno2=0--记录奇数数量
+	for i=1,4 do
+		ctno2 = ctno2 + __fmod(tc[i],2)
+	end
+	--2个以上数量非偶数 排除
+	if(ctno2>2) then
+		return false
+	end
+	------------------------------------
+	local rr={}
+	--统计所有奇数牌
+	local tj={}
+	for i=1,4 do
+		local v = t[i]
+		local j=1
+		while v>0 do
+			if(__fmod(v,2)==1) then
+				tj[#tj+1] = __combine(i,j)
+			end
+			v = __floor(v/10) 
+			j = j + 1
+		end
+		--超过2个单排
+		if(#tj>2) then
+			return false
+		end
+	end
+
+	if(#tj==2) then
+		--两张中出任意一张
+		rr[1] = tj[1].."--"..tj[2]
+		rr[2] = tj[2].."--"..tj[1]
+	elseif(#tj==0) then
+		--已经是胡牌，任意一张
+		for i=1,4 do
+			local v = t[i]
+			local j=1
+			while v>0 do
+				if(__fmod(v,10)>0) then
+					local vv = __combine(i,j)
+					rr[#rr+1] = vv.."--"..vv
+				end
+				v = __floor(v/10)
+				j = j + 1
+			end
+		end
+	end
+	------------------------------------
 	if(#rr>0) then
 		return rr
 	end
 end
 
-function CMD.hu2(t)
-	local flag = true
-	local jc = 0
-	-- util.dump(t,"CMD.hu")
-	-- skynet.error(mjlib.i2str(t[1]))
-	-- skynet.error(mjlib.i2str(t[2]))
-	-- skynet.error(mjlib.i2str(t[3]))
-	-- skynet.error(mjlib.i2str(t[4]))
-	for i=1,4 do
-		if t[i]>0 then
-			-- local key = mjlib.i2str(t[i])
-			local key = t[i]
-			local r  = CMD.get(i,key)
-			if(not r) then
-				flag = false
-				break
-			else
-				jc = jc + r
-			end
-		end
-	end
-	return flag and jc==1
-end
-
-function CMD.hu(pai)
-	local t,_= __tongji(pai)
-	local flag = true
-	local jc = 0
-	-- util.dump(t,"CMD.hu")
-	-- skynet.error(mjlib.i2str(t[1]))
-	-- skynet.error(mjlib.i2str(t[2]))
-	-- skynet.error(mjlib.i2str(t[3]))
-	-- skynet.error(mjlib.i2str(t[4]))
-	for i=1,4 do
-		if t[i]>0 then
-			-- local key = mjlib.i2str(t[i])
-			local key = t[i]
-			local r  = CMD.get(i,key)
-			if(not r) then
-				flag = false
-				break
-			else
-				jc = jc + r
-			end
-		end
-	end
-	return flag and jc==1
-end
-local function test()
+local function testA()
 	local ttt ={
-		1,2,3, 4,5,6, 31,31,31, 11,12,13, 20,20
+		-- 1,2,3, 4,5,6, 31,31,31, 11,12,13, 20,20
+		-- 11,12,13, 30,30,30, 7,8,9, 21,22,23, 6,6
+		1,1,2,2,3,3, 4,4, 5,5,6,6, 7,8
 	}
 	local ta = skynet.now()
 	local r
 	for i=1,100000 do
-		r = CMD.hu(ttt)
+		r = CMD.huA(ttt)
 	end
 	local tb = skynet.now()
-	skynet.error("call CMD.hu = ",r , " tm =",(tb-ta)*10)
+	skynet.error("call CMD.huA = ",r , " tm =",(tb-ta)*10)
 	skynet.sleep(10)
 
 	local ta = skynet.now()
@@ -310,20 +397,20 @@ local function test()
 	for i=1,100000 do
 		t[1] = t[1]+BB[3]
 		t[1] = t[1]-BB[3]
-		r = CMD.hu2(t)
+		r = CMD.huA__(t)
 	end
 	local tb = skynet.now()
-	skynet.error("call CMD.hu2 = ",r , " tm =",(tb-ta)*10)
+	skynet.error("call CMD.huA__ = ",r , " tm =",(tb-ta)*10)
 	skynet.sleep(10)
-
+	---------------------------------------------------------
 	local ta = skynet.now()
 	local r
 	for i=1,10000 do
-		r = CMD.ting(ttt)
+		r = CMD.tingA(ttt)
 	end
 	local tb = skynet.now()
-	skynet.error("call CMD.ting tm =",(tb-ta)*10)
-	util.dump(r,'CMD.ting')
+	skynet.error("call CMD.tingA tm =",(tb-ta)*10)
+	util.dump(r,'CMD.tingA')
 	skynet.sleep(10)
 
 	local ta = skynet.now()
@@ -332,11 +419,63 @@ local function test()
 	for i=1,10000 do
 		t[2] = t[2]+BB[9]
 		t[2] = t[2]-BB[9]
-		r = CMD.ting2(t,tc)
+		r = CMD.tingA__(t,tc)
 	end
 	local tb = skynet.now()
-	skynet.error("call CMD.ting2 tm =",(tb-ta)*10)
-	util.dump(r,'CMD.ting2')	
+	skynet.error("call CMD.tingA__ tm =",(tb-ta)*10)
+	util.dump(r,'CMD.tingA__')
+end
+local function test7D()
+	---------------------------------------------------------
+	---------------------------------------------------------
+	---------------------------------------------------------
+	local ttt ={
+		-- 1,2,3, 4,5,6, 31,31,31, 11,12,13, 20,20
+		-- 1,1,1,1, 11,11,11,11, 30,30,30,30, 7,7
+		1,1,2,2,3,3,4,4,5,5,6,6, 7,8
+	}
+	local ta = skynet.now()
+	local r
+	for i=1,100000 do
+		r = CMD.hu7D(ttt)
+	end
+	local tb = skynet.now()
+	skynet.error("call CMD.hu7D = ",r , " tm =",(tb-ta)*10)
+	skynet.sleep(10)
+
+	local ta = skynet.now()
+	local r
+	local t,tc= mjlib.tongji(ttt)
+	for i=1,100000 do
+		t[1] = t[1]+BB[3]
+		t[1] = t[1]-BB[3]
+		r = CMD.hu7D__(t,tc)
+	end
+	local tb = skynet.now()
+	skynet.error("call CMD.hu7D__ = ",r , " tm =",(tb-ta)*10)
+	skynet.sleep(10)
+	---------------------------------------------------------
+	local ta = skynet.now()
+	local r
+	for i=1,10000 do
+		r = CMD.ting7D(ttt)
+	end
+	local tb = skynet.now()
+	skynet.error("call CMD.ting7D tm =",(tb-ta)*10)
+	util.dump(r,'CMD.ting7D')
+	skynet.sleep(10)
+
+	local ta = skynet.now()
+	local r
+	local t,tc= mjlib.tongji(ttt)
+	for i=1,10000 do
+		t[2] = t[2]+BB[8]
+		t[2] = t[2]-BB[8]
+		r = CMD.ting7D__(t,tc)
+	end
+	local tb = skynet.now()
+	skynet.error("call CMD.ting7D__ tm =",(tb-ta)*10)
+	util.dump(r,'CMD.ting7D__')
 end
 
 local function test_table()
@@ -384,7 +523,6 @@ local function test_table()
 	local tb = skynet.now()
 	skynet.error("call table[s2] tm =",(tb-ta)*10)
 	skynet.sleep(10)
-
 end
 
 skynet.start(function()
@@ -395,7 +533,8 @@ skynet.start(function()
 	-- skynet.error("majiang builder end",(tb-ta)*10)
 	-- zipai = nil
 	-- fengpai = nil
-	skynet.fork(test)
+	skynet.fork(testA)
+	skynet.fork(test7D)
 	-- skynet.fork(test_table)
 	skynet.dispatch('lua',function(session, source, cmd, ...)
 		if(CMD[cmd]) then
