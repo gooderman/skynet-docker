5129ead905e02fed974962aa7e179291aafa4fe3
Jeep <1121838283@qq.com>
2018-02-17 13:02:35 +0800

    docker run succ docker diff container connect succ

--- docker/skynet-alpine/start.sh
+++ docker/skynet-alpine/start.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 echo "---------start skynet---------"
 echo $1 $2 
+if [ $# == 0 ]; then
 echo 'ping mysql'
 ping mymysql -c 4
-if [ $# == 0 ]; then
 /app/skynet/skynet /data/GMA/config.lua
 else
-/app/skynet/skynet /data/GMA/config.lua & 
-sleep 2s
+# /app/skynet/skynet /data/GMA/config.lua & 
+# sleep 2s
+echo 'ping myskynet'
+ping myskynet -c 4
 /app/skynet/skynet /data/GMA/config_test.lua
 fi
 echo "-----------------------------"
--- start_docker.sh
+++ start_docker.sh
@@ -8,5 +8,12 @@ echo 'wait 2s for mysql startup'
 sleep 2s
 cd ../..
 cd docker/skynet-alpine
-docker run -it --rm -v `pwd`:/data -p 8000:8000 -p 8080:8080 -p 8888:8888 --network skynet-net skynet_alpine:v1 '1'
+docker stop myskynet
+docker rm myskynet
+docker run -d --rm --name myskynet -v `pwd`:/data -p 8000:8000 -p 8080:8080 -p 8888:8888 --network skynet-net skynet_alpine:v1
+echo 'wait 2s for myskynet startup'
+sleep 2s
+docker stop myskynet_test
+docker rm myskynet_test
+docker run -it --rm --name myskynet_test -v `pwd`:/data  --network skynet-net skynet_alpine:v1 '2'
 cd ../..
